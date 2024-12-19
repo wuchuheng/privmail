499f4a3269704a42dabbf468e566f0991affa958
wuchuheng <root@wuchuheng.com>
2024-12-20 00:09:40 +0800

    feat(dovecot): make the logs to print

--- src/dovecot/start-dovecot.sh
+++ src/dovecot/start-dovecot.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# 1. Create vmail user and group.
+# 1. Create the vmail group and user if they don't exist.
 # TODO: Make the user id and group id as environment variables in the docker-compose file.
-groupadd -g ${VMAIL_GROUP_ID:-5000} vmail
-useradd -u ${VMAIL_USER_ID:-5000} -g vmail -d /var/mail vmail
+if ! getent group vmail > /dev/null; then   
+    groupadd -g ${VMAIL_GROUP_ID:-5000} vmail
+fi
+
+if ! id -u vmail > /dev/null 2>&1; then
+    useradd -u ${VMAIL_USER_ID:-5000} -g vmail -d /var/mail vmail
+fi
 
 # 2. Set the directory permissions.
 chown -R vmail:vmail /var/mail
 chmod 2770 /var/mail
 
-# 3. Set up the shared socket directory
-mkdir -p /var/spool/postfix/private
-chown postfix:postfix /var/spool/postfix/private
-chmod 710 /var/spool/postfix/private
-
-# 4. Start Dovecot.
+# 3. Start Dovecot.
 dovecot -F

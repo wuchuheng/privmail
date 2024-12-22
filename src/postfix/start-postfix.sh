070abeeb76bda34e068235254a7c6938b5f4fa2d
wuchuheng <root@wuchuheng.com>
2024-12-22 17:57:57 +0800

    tmp

--- src/postfix/start-postfix.sh
+++ src/postfix/start-postfix.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # 1. If the vmail group or user is not found, create it. 
-if ! getent group vmail > /dev/null; then
-    groupadd -g ${VMAIL_GROUP_ID:-5000} vmail;
-    echo "Group vmail created with ID ${VMAIL_GROUP_ID:-5000}."
-fi
-
 if ! id -u vmail > /dev/null 2>&1; then
     useradd -u ${VMAIL_USER_ID:-5000} -g vmail -d /var/mail vmail;
     echo "User vmail created with ID ${VMAIL_USER_ID:-5000}."
+fi
+
+if ! getent group vmail > /dev/null; then
+    groupadd -g ${VMAIL_GROUP_ID:-5000} vmail;
+    echo "Group vmail created with ID ${VMAIL_GROUP_ID:-5000}."
 fi
 
 # Fix permissions for main.cf
@@ -28,6 +28,14 @@ chmod -R 750 /var/spool/postfix/maildrop
 chown -R postfix:postdrop /var/spool/postfix/maildrop
 ls -ahl /var/spool/postfix/maildrop
 chmod -R 750 /var/spool/postfix/pid
+
+# Set permissions for vmaps file
+chown postfix:postfix /etc/postfix/vmaps
+chmod 644 /etc/postfix/vmaps
+
+# Set permissions for /var/mail
+chown -R vmail:vmail /var/mail
+chmod -R 2770 /var/mail
 
 # Start Postfix
 /usr/sbin/postfix start-fg

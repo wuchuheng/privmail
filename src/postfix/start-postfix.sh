c84c619f336471757319588bc65eb7a79b7e68e3
wuchuheng <root@wuchuheng.com>
2024-12-24 15:50:36 +0800

    fix(postfix): correct permissions in postfix container

--- src/postfix/start-postfix.sh
+++ src/postfix/start-postfix.sh
@@ -6,23 +6,13 @@ DIR="$(cd "$(dirname "$0")" && pwd)"
 
 
 # 1. Check the necessary environment variables
-env_check MAIL_UID MAIL_GID MAIL_USER_NAME MAIL_DOMAIN || exit 1
+env_check MAIL_UID MAIL_DOMAIN || exit 1
 
 # 2. Check if the user exists or create it. 
-user_exists_or_create ${MAIL_USER_NAME} ${MAIL_UID} || exit 1
+user_exists_or_create postfix ${MAIL_UID} || exit 1
 
 # 3. Convert the template to the main.cf file
-envsubst '${MAIL_DOMAIN} ${MAIL_USER_NAME}' < ${DIR}/config/main.cf.template > /etc/postfix/main.cf
+envsubst '${MAIL_DOMAIN}' < ${DIR}/config/main.cf.template > /etc/postfix/main.cf
 
-# 4. Set correct permissions for Postfix directories
-postfix_dirs="/var/spool/postfix /var/lib/postfix"
-for dir in $postfix_dirs; do
-    chown -R ${MAIL_USER_NAME}:${MAIL_USER_NAME} $dir
-    chmod -R 0700 $dir
-done
-
-
-# Enable verbose logging and switch to foreground mode
-log INFO "=== Postfix Configuration Check ==="
-postconf -n
+# 5. Start Postfix
 /usr/sbin/postfix start-fg -v

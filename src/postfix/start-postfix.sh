00a305f109216b2cd18c27045f81d1288077d336
wuchuheng <root@wuchuheng.com>
2024-12-25 03:10:28 +0800

    fix(postfix): fix the issue that a error is encounterd when a connection based over ssl

--- src/postfix/start-postfix.sh
+++ src/postfix/start-postfix.sh
@@ -14,5 +14,17 @@ user_exists_or_create postfix ${MAIL_UID} || exit 1
 # 3. Convert the template to the main.cf file
 envsubst '${MAIL_DOMAIN}' < ${DIR}/config/main.cf.template > /etc/postfix/main.cf
 
-# 5. Start Postfix
+# 4. Generate the aliases database.
+cat /app/src/postfix/config/aliases > /etc/postfix/aliases 
+postalias /etc/postfix/aliases
+log INFO "Generated the aliases database."
+
+# 5. Genreate the vmaps.db file if it doesn't exist.
+vmaps_file="/etc/postfix/vmaps"
+if [ ! -f "$vmaps_file.db" ]; then
+    postmap "$vmaps_file"
+    log INFO "Generated the vmaps.db file."
+fi
+
+# 6. Start Postfix
 /usr/sbin/postfix start-fg -v
